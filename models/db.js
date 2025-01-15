b63cae7564b2a6536de4feb40ef3f158bb8a0b9a
Your Name <you@example.com>
2025-01-15 10:41:30 +0530

    chnage layout

--- models/db.js
+++ models/db.js
@@ -1,12 +1,10 @@
 const mysql = require('mysql2/promise');
 
-// Create a pool of MySQL connections
 const pool = mysql.createPool({
-  host: 'localhost',      // MySQL server address
-  user: 'root',           // Username to connect to MySQL
-  password: '',           // Password for the MySQL user (leave empty if none)
-  database: 'jvnbs',      // The database name you're connecting to
+  host: 'localhost',  
+  user: 'root',  
+  password: '',    
+  database: 'pti',  
 });
 
-// Export the pool to be used in other files
 module.exports = pool;
